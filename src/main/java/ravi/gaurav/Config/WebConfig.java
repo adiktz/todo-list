1b9ced82ac90970d1e8c64f8a4f8970339e721d9
gaurav <gauravraviji@gmail.com>
2020-06-16 03:03:10 +0530

    Configured dispatcher Servlet

---
+++ src/main/java/ravi/gaurav/Config/WebConfig.java
@@ -0,0 +1,11 @@
+package ravi.gaurav.Config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "ravi.gaurav")
+public class WebConfig {
+}
---
+++ src/main/java/ravi/gaurav/Main.java
@@ -0,0 +1,4 @@
+package ravi.gaurav;
+
+public class Main {
+}
