8a7c8f01b65faf34dac26a0686be7647f46214f0
gaurav <gauravraviji@gmail.com>
2020-06-16 20:15:01 +0530

    ToDo data store added

--- src/main/java/ravi/gaurav/controller/DemoController.java
+++ src/main/java/ravi/gaurav/controller/DemoController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import ravi.gaurav.service.DemoService;
 
@@ -29,8 +30,9 @@ public class DemoController {
 
     // http://localhost:8080/Todo-List/welcome
     @GetMapping("welcome")
-    public String welcome(Model model) {
-        model.addAttribute("helloMessage", demoService.getHelloMessage("Gaurav"));
+    public String welcome(@RequestParam String user, @RequestParam int age, Model model) {
+        model.addAttribute("helloMessage", demoService.getHelloMessage(user));
+        model.addAttribute("age", age);
         log.info("Model = {}", model);
         return "welcome";
     }
